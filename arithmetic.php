8c48e7abd23d3c839bdbd655e4568bbde8c8f4b4
Chris Davila <cmd1603@gmail.com>
2016-07-06 15:37:09 -0500

    saving changes to arithmetic exericise, adding an error message if user tries to divide by 0

--- arithmetic.php
+++ arithmetic.php
@@ -2,33 +2,45 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a + $b;
+	} else
+    	return "Error, both arguments must be numbers\n";
 }
-echo add(5, 4). PHP_EOL;
+echo add("string", 4). PHP_EOL;
 
 function subtract($a, $b)
 {
-    return $a - $b;
-
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a - $b;
+    } else
+    	return "Error, both arguments must be numbers\n";
 }
 echo subtract(5, 4). PHP_EOL;
 
 function multiply($a, $b)
 {
-    return $a * $b;
-
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a * $b;
+    } else
+    	return "Error, both arguments must be numbers\n";
 }
 echo multiply(5, 4). PHP_EOL;
 
 function divide($a, $b)
 {
-    return $a / $b;
-
+	if (is_numeric($a) && is_numeric($b) && $b != 0) {
+    	return $a / $b;
+    } else
+    	return "Error, both arguments must be numbers and divisor not equal to zero.\n";
 }
-echo divide(20, 4). PHP_EOL;
+echo divide(20, -2). PHP_EOL;
 
 function modulus($a, $b)
 {
-	return $a % $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a % $b;
+	} else
+		return "Error, both arguments must be numbers\n";	
 }
-echo modulus(10, 3). PHP_EOL;
+echo modulus(10, "string"). PHP_EOL;
