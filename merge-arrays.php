d1468ba704f014b19e8d87c09b4a88f2e22f804c
Chris Davila <cmd1603@gmail.com>
2016-07-08 16:03:36 -0500

    saving first changes to merge-arrays exercise-merge done

---
+++ merge-arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function combineArrays ($array1, $array2)
+{
+	$array3 = [];
+
+	foreach ($array1 as $key => $value) {
+		if($array2[$key] !== $array1[$key]) {
+
+			array_push($array3, $array1[$key]);
+			array_push($array3, $array2[$key]);
+		} else {
+
+			array_push($array3, $value);
+		}
+	} //end of foreach function
+
+	return var_dump($array3);
+} //end of combineArrays
+
+echo combineArrays($names, $compare) . PHP_EOL;
+
