4cef2227d5815326702100f660f533d9df919d1b
Chris Davila <cmd1603@gmail.com>
2016-07-11 14:10:46 -0500

    saving contact parser exercise with all changes done

--- contact_parser.php
+++ contact_parser.php
@@ -2,7 +2,27 @@
 
 function parseContacts($filename)
 {
-    $contacts = array();
+    $contactsArray = array();
+    $handle = fopen($filename, 'r');
+    $contents = fread($handle, filesize($filename));
+    $contentsArray = explode("\n", trim($contents));
+
+    foreach ($contentsArray as $value) {
+    	$divide = explode("|", $value);
+
+    	$name = $divide[0];
+    	$phone = $divide[1];
+    	$areaCode = substr($phone, 0, 3);
+    	$prefix = substr($phone, 3, 3);
+    	$last = substr($phone, -4);
+    	$phone = $areaCode.'-'.$prefix.'-'.$last;
+    	$contacts[] = [
+    		'name' => $name,
+    		'phone' => $phone,
+    	];
+    }
+
+    fclose($handle);
 
     // todo - read file and parse contacts
 
