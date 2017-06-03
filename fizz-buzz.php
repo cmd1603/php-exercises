2a210c89b23c2efe14c1672730fde272c0bad5a4
Chris Davila <cmd1603@gmail.com>
2017-06-03 15:49:08 -0500

    Added missing exercises and updated others

--- fizz-buzz.php
+++ fizz-buzz.php
@@ -2,24 +2,25 @@
 
 // Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
-//for ($i = 1; $i <= 100; $i++) {
-//	if($i % 3 == 0 && $i % 5 == 0){
-//		echo "FizzBuzz \n";
-//	} elseif ($i %3 == 0) {
-//		echo "Fizz \n";
-//	} elseif ($i % 5 == 0) {
-//		echo "Buzz \n";
-//	}else {
-//		echo "$i \n" ;
-//	}
-//}
-
-$count = 2;
-for ($i=0; $i < 2; $i++) {
-    for ($j=0; $j < 2; $j++) {
-        echo $i . $j . PHP_EOL;
-        if ($j == 1) {
-            break $count;
-        }
-    }
+for ($i = 1; $i <= 100; $i++) {
+	if($i % 3 == 0 && $i % 5 == 0){
+		echo "FizzBuzz \n";
+	} elseif ($i % 3 == 0) {
+		echo "Fizz \n";
+	} elseif ($i % 5 == 0) {
+		echo "Buzz \n";
+	} else {
+		echo "$i \n" ;
+	}
 }
+
+// $count = 2;
+// for ($i=0; $i < 2; $i++) {
+//     for ($j=0; $j < 2; $j++) {
+//         echo $i . $j . PHP_EOL;
+//         if ($j == 1) {
+//             break $count;
+//         }
+//     }
+// }
+
--- foreach.php
+++ foreach.php
@@ -11,25 +11,13 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 
-// foreach ($things as $thing) {
-// 		if (is_numeric($thing)) {
-// 			echo "{$thing} is a number \n";
-// 		} elseif (is_string($thing)) {
-// 			echo "{$thing} is a string \n";
-// 		} elseif (is_bool($thing)) {
-// 			echo "{$thing} is a boolean \n";
-// 		} elseif (is_array($thing)) {
-// 			echo "{$thing} is an array \n";
-// 		} elseif (is_integer($thing)) {
-// 			echo "{$thing} is an integer \n";
-// 		}
-
-// }
-
 foreach ($things as $thing) {
-	if (is_scalar($thing)) {
-		echo $thing . PHP_EOL;
+	if (is_array($thing)) {
+		echo "Inner array:\n";
+		foreach ($thing as $number) {
+			echo "$number " . PHP_EOL;
+		}
+	} else {
+	echo $thing.PHP_EOL;
 	}
-} else {
-	echo $thing . PHP_EOL;
 }
